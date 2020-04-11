f38d0b0eaffc1b9b437b34f12598fd091e40d197
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-04-11 08:20:32 -0300

    chore: config routes

---
+++ src/Routes/Route.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/orders" />;
+  }
+
+  return (
+    <Route {...rest} component={Component} />
+  )
+}
---
+++ src/Routes/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+
+import LogIn from '../pages/LogIn';
+
+import Order from '../pages/Order';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={LogIn} />
+
+      <Route path="/orders" component={Order} isPrivate />
+    </Switch>
+  )
+}
