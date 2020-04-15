3da845605647470d767b68b605331332b7c48676
NakaokaGabriel <gabrielnakaoka_99@hotmail.com>
2020-04-14 23:57:39 -0300

    feature: authenticated login styles

--- src/pages/LogIn/index.js
+++ src/pages/LogIn/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Form, FormGroup } from './styles';
 
 import logo from '~/assets/logo.svg';
 
@@ -8,7 +8,21 @@ export default function LogIn() {
   return (
     <Container>
       <img src={logo} alt="fastfeet" />
-      <Form />
+      <Form>
+        <FormGroup>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="email">SEU E-MAIL</label>
+          <input type="text" id="email" placeholder="exemplo@email.com" />
+        </FormGroup>
+
+        <FormGroup>
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label htmlFor="password">SUA SENHA</label>
+          <input type="password" id="password" placeholder="*************" />
+        </FormGroup>
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
     </Container>
   );
 }
