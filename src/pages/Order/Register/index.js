2954f9424b9cabd84f2caefb8171c6bd7c75f4e2
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-04-29 20:06:07 -0300

    feature: create registration page

--- src/pages/Order/Register/index.js
+++ src/pages/Order/Register/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
 import { Container } from '~/components/Container/styles';
-import Header from '~/components/Header/Default';
 import { Background } from '~/components/Background/styles';
+import { FormGroup, InputGroup } from '~/components/Form/styles';
+
+import Header from '~/components/Header/Default';
 
 export default function Register() {
   return (
     <Container>
       <Header title="Cadastro de encomendas" />
       <Background background>
-        <h1>hello</h1>
+        <FormGroup>
+          <InputGroup>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="destinatario">Destinatário</label>
+            <input type="text" />
+          </InputGroup>
+
+          <InputGroup>
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="entregador">Entregador</label>
+            <input type="text" />
+          </InputGroup>
+
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          {/* <label htmlFor="produto">Nome do produto</label>
+          <input type="text" /> */}
+        </FormGroup>
       </Background>
     </Container>
   );
