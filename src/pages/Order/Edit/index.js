614d2e3f0c46dafc969c533275ce5c69e84a0e4a
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-05-02 06:48:44 -0300

    feature: create styles edit orders

--- src/pages/Order/Edit/index.js
+++ src/pages/Order/Edit/index.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
 // import { Container } from './styles';
+import { Container } from '~/components/Container/styles';
+import Header from '~/components/Header/Default';
+import { Background } from '~/components/Background/styles';
+import { FormGroup, InputGroup } from '~/components/Form/styles';
 
 export default function Edit() {
-  return <div />;
+  return (
+    <Container>
+      <Header title="Edição de encomendas" />
+      <Background background>
+        <FormGroup>
+          <InputGroup>
+            <div>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label htmlFor="destinatario">Destinatário</label>
+              <input type="text" placeholder="Ludwig van Beethoven" />
+            </div>
+
+            <div>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label htmlFor="entregador">Entregador</label>
+              <input type="text" placeholder="Ludwig van Beethoven" />
+            </div>
+          </InputGroup>
+
+          <InputGroup>
+            <div>
+              {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+              <label htmlFor="produto">Nome do produto</label>
+              <input type="text" placeholder="Ludwig van Beethoven" />
+            </div>
+          </InputGroup>
+        </FormGroup>
+      </Background>
+    </Container>
+  );
 }
