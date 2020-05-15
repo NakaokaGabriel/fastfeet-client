51c83c88ecbc0650ed9fb7f444adb0973a06dbc8
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-05-14 21:26:53 -0300

    feature: listing actions

--- src/pages/Order/styles.js
+++ src/pages/Order/styles.js
@@ -4,20 +4,23 @@ export const Actions = styled.div`
   position: relative;
 
   .actions {
+    overflow: ${(props) => (props.actions ? 'unset' : 'hidden')};
     position: absolute;
     left: -30px;
     width: 150px;
+    z-index: 100;
 
     &::after {
       content: '';
       position: absolute;
       top: -5px;
       left: 50%;
-      box-shadow: 0px 0px 5px #00000026;
+      background: #fff;
+      box-shadow: -1px -1px 2px 0px #00000026;
       width: 10px;
       height: 10px;
       transform: rotate(45deg);
-      z-index: 99;
+      z-index: 1000;
     }
 
     ul {
@@ -28,7 +31,7 @@ export const Actions = styled.div`
       text-align: left;
       background: #fff;
       box-shadow: 0px 0px 2px #00000026;
-      z-index: 100;
+      border-radius: 4px;
 
       li {
         margin: 0 10px;
