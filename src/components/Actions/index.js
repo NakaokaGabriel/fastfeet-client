7d8d7867e0ad84a2e17082a1fc8660bcf24b9a7d
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2020-10-25 22:44:47 -0300

    task: adjusted click and leave/out event

--- src/components/Actions/index.js
+++ src/components/Actions/index.js
@@ -1,14 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { MdMoreHoriz } from 'react-icons/md';
 
 import { Action } from './styles';
 
 function Actions({ children }) {
+  const outsideClick = useRef(null);
+
   const [action, setAction] = useState(false);
 
+  const handleClickOutside = useCallback(
+    (event) => {
+      if (!outsideClick.current.contains(event.target)) {
+        setAction(false);
+      }
+    },
+    [outsideClick]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   return (
-    <Action isAction={action}>
+    <Action
+      ref={outsideClick}
+      isAction={action}
+      onMouseLeave={() => setAction(false)}
+      onMouseOut={() => {
+        setAction(true);
+      }}
+      onBlur={() => {
+        setAction(false);
+      }}
+    >
       <button type="button" onClick={() => setAction(!action)}>
         <MdMoreHoriz color="#C6C6C6" size={32} />
       </button>
